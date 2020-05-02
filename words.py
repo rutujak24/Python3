a8b27578881b0c55f4090d6a162072049a435955
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-05-02 11:45:39 +0530

    Update and rename 2.py to words.py

--- words.py
+++ words.py
@@ -1,5 +1,5 @@
-The variable sentence stores a string. Write code to determine how many words in sentence start and end with the 
-same letter, including one-letter words. Store the result in the variable same_letter_count.
+'''The variable sentence stores a string. Write code to determine how many words in sentence start and end with the 
+same letter, including one-letter words. Store the result in the variable same_letter_count.'''
 
 sentence = "students flock to the arb for a variety of outdoor activities such as jogging and picnicking"
 same_letter_count = 0
